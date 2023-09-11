a8b685164fde39187cb8ef6420725bbee4c71665
archit-mriirs <architsinghal_mriirs@outlook.com>
2023-09-11 20:05:09 +0530

    (feat): Added lift code

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-const liftButton = document.querySelector('.upBotton')
+// const liftButton = document.querySelector('.upBotton')
 const generateButton = document.querySelector('.generateLift')
 const formContainer = document.querySelector('.formContainer')
 const mainContainer = document.querySelector('.main')
@@ -7,9 +7,12 @@ const numberOfLifts = document.getElementById('liftNumberInput')
 const numberOfFloors = document.getElementById('floorNumberInput')
 const building = document.querySelector('.building')
 
+let floorsQueue = []
+let liftsState = []
+
 generateButton.addEventListener('click', (event) => {
     event.preventDefault()
-    console.log('generateButton is pressed')
+    // console.log('generateButton is pressed')
     formContainer.style.visibility = 'hidden'
     const newDiv = document.createElement("div");
 
@@ -20,10 +23,11 @@ generateButton.addEventListener('click', (event) => {
     formContainer.style.height = "0px"
     formContainer.style.width = "0px"
     mainContainer.appendChild(newDiv);
-    console.log("lifts", numberOfLifts.value)
-    console.log("floors", numberOfFloors.value)
+    // console.log("lifts", numberOfLifts.value)
+    // console.log("floors", numberOfFloors.value)
 
     renderFloorsAndLifts(numberOfFloors.value, numberOfLifts.value)
+
     
 })
 
@@ -54,23 +58,165 @@ function renderFloorsAndLifts(numberOfFloors, numberOfLifts) {
         downButton.textContent = "Down"
         floorName.textContent = `Floor ${numberOfFloors - i}`
 
+        upButton.id = `Floor ${numberOfFloors - i}`
+        downButton.id = `Floor ${numberOfFloors - i}`
+        upButton.addEventListener("click", handleClick)
+        downButton.addEventListener("click", handleClick)
+
     }
     const liftContainer = document.createElement("div")
     liftContainer.classList.add("liftContainer")
     building.appendChild(liftContainer)
-    for(let i=0; i<numberOfLifts; i++) {
+    for(let i=1; i<=numberOfLifts; i++) {
         const lift = document.createElement("div")
         lift.classList.add("lift")
+        lift.id = `Lift ${i}`
         liftContainer.appendChild(lift)
+        lift.innerText = `Lift ${i}`
+        lift.innerHTML = `<div class=liftOverlay id=liftOverlay${i}></div>`
     }
+
+    initializeLiftState(numberOfLifts)
+    console.log("This is the lifts state", liftsState)
      
 }
 
-liftButton.addEventListener('click', () => {
-    console.log("This button was clicked")
-    lift.style.transform = 'translateY(-24em)'
-    lift.style.transition = 'all 2s ease'
-})
+function getLiftOverlay(floorNumber) {
+    return document.getElementById(`liftOverlay${floorNumber}`)
+}
+
+function initializeLiftState(numberOfLifts) {
+    for(let i=1; i<=numberOfLifts; i++) {
+        liftsState.push({
+            liftNumber: i,
+            currentFloorNumber: 1,
+            isOccupied: false
+        })
+    }
+}
+
+function getFreeLift() {
+    return liftsState?.find(lift => !lift?.isOccupied)
+}
+
+function getLiftFromLiftNumber(liftNumber) {
+    return document.getElementById(`Lift ${liftNumber}`)
+}
+
+function handleLiftClick(event) {
+    console.log("Called",event.target)
+    
+}
+console.log("This is the floors queue", floorsQueue)
+
+async function handleClick(event) {
+    //check which floor's button is clicked
+    const buttonPressed = event.target
+    const floorNumber = buttonPressed?.id?.charAt(buttonPressed?.id?.length - 1)
+    // enqueue the floor number
+    floorsQueue.push(floorNumber)
+
+    let availableLifts = liftsState?.filter(lift => !lift.isOccupied)
+
+    if(availableLifts.length === 0) {
+        return
+    }
+
+    console.log("This is the floorsQueue", floorsQueue)
+
+   await handleLiftMovement()
+    
+}
+
+async function handleLiftMovement() {
+    if(floorsQueue && floorsQueue.length > 0) {
+        // let queue = floorsQueue
+        //get the enqueued floor number
+        const targetFloor = floorsQueue.shift()
+        console.log("Shift Called")
+
+        // get Empty Lift Number. TODO: Modify this to get the nearest lift
+        const freeLift = getFreeLift()
+
+        if(freeLift) {
+            // get Empty Lift Element
+        const liftElement = getLiftFromLiftNumber(freeLift?.liftNumber)
+
+        //calculate the distance between the free lift's current floor and the targetFloor
+        const distance = getTravelDistance(freeLift, targetFloor)
+
+        
+
+        //update lift state to be busy
+        updateLiftState(freeLift, targetFloor, isOccupied = true)
+        const door = getLiftOverlay(freeLift?.liftNumber)
+        
+        if(distance === 0) {
+            await handleDoorOpening(door, freeLift, targetFloor)
+        }else{
+//send the free lift to this floor
+        // setTimeout(moveLift(liftElement, distance), 2000 * distance)
+        moveLift(liftElement, distance, targetFloor)
+        await setWaitingTime(2 * distance)
+        await handleDoorOpening(door, freeLift, targetFloor)
+        }
+        
+    }
+        // floorsQueue = queue
+    }
+    
+}
+
+async function handleDoorOpening(door, freeLift, targetFloor) {
+    openDoor(door)
+    await setWaitingTime(2.5)
+    closeDoor(door)
+    await setWaitingTime(2.5)
+    updateLiftState(freeLift, targetFloor, isOccupied = false)
+    await handleLiftMovement()
+}
+
+function openDoor(door) {
+    // door.classList.remove("closeLift")
+    door.classList.add("openLift")
+    door.classList.remove("closeLift")
+}
+
+function setWaitingTime(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000))
+}
+
+function closeDoor(door) {
+    door.classList.add("closeLift")
+    door.classList.remove("openLift")
+}
+
+function updateLiftState(freeLift, targetFloor, isOccupied) {
+    freeLift.currentFloorNumber = targetFloor
+    freeLift.isOccupied = isOccupied
+}
+
+
+function getTravelDistance(freeLift, floor) {
+    const currentFloorOfTheFreeLift = freeLift?.currentFloorNumber
+    const absoluteDistance = Math.abs( floor - currentFloorOfTheFreeLift )
+    return absoluteDistance
+}
+
+function moveLift(lift, distance, targetFloor) {
+    // const distance = getTravelDistance(lift, floorNumber)
+    // console.log("This is the distance to be travelled", distance)
+    lift.style.transform = `translateY(-${(targetFloor - 1) * 130}px)`
+    lift.style.transition = `all ${distance * 2}s ease`
+
+}
+
+
+// liftButton.addEventListener('click', () => {
+//     console.log("This button was clicked")
+//     lift.style.transform = 'translateY(-24em)'
+//     lift.style.transition = 'all 2s ease'
+// })
 
 
 
